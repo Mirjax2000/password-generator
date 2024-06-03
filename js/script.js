ed98a562ee437ca6cb79cf491d4cbffd732b7a97
Vigo <miroslav.viktorin@seznam.cz>
2024-06-03 20:40:43 +0200

    aktiv slider

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@
 // -----------------
 
 $(function () {
-   
+    let slider = $('#myRange'),
+        output = $('#demo');
+
+    // slider
+    output.html(slider.val());
+    slider.on('input', function () {
+        output.html($(this).val());
+    });
 });
-
-
